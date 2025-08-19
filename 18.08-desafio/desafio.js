2a5cb7a878816397419668007632355c0d90521a
Renata <renata.rodriguesdasilva@outlook.com>
2025-08-18 23:52:33 -0300

    feat: update˜

---
+++ 18.08-desafio/desafio.js
@@ -0,0 +1,23 @@
+const somar = () => {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+  alert(numero1 + numero2);
+};
+
+const subtrair = () => {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+  alert(numero1 - numero2);
+};
+
+const multiplicar = () => {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+  alert(numero1 * numero2);
+};
+
+const dividir = () => {
+  let numero1 = Number(document.getElementById("numero1").value);
+  let numero2 = Number(document.getElementById("numero2").value);
+  alert(numero1 / numero2);
+};
